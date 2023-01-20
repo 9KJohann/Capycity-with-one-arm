228038c2565326d27bc9e3ccf21b6a6d39da91ce
Johann Dietrich <johann.dietrich@stud.hs-coburg.de>
2023-01-20 20:37:25 +0100

    kapitel 1

---
+++ BuildingType.h
@@ -0,0 +1,11 @@
+#ifndef CAPYCITY_BUILDINGTYPE_H
+#define CAPYCITY_BUILDINGTYPE_H
+
+enum BuildingType : char {
+    EMPTY = 'E',
+    WATER_ENERGY_PLANT = 'W',
+    SOLAR_ENERGY_PLANT = 'S',
+    WIND_ENERGY_PLANT = 'I',
+};
+
+#endif //CAPYCITY_BUILDINGTYPE_H
